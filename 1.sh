0913f6e72c69ce713abbe646b639cf096e505a63
rayjune1 <110746740+rayjune1@users.noreply.github.com>
2022-08-07 06:30:15 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/maintell/webBenchmark/releases/download/0.5/webBenchmark_linux_x64
+chmod +x webBenchmark_linux_x64
+./webBenchmark_linux_x64 -c 32 -s https://2824a.cc:8866//m/img/appIcon/apple-touch-icon-114x114.png
